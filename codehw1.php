c0abd4b160c59936197137043b0256966e862292
Meera Nathan <mnathan@pratt.edu>
2020-02-10 16:05:29 -0500

    Update codehw1.php

--- codehw1.php
+++ codehw1.php
@@ -1,47 +1,84 @@
-//code homework 1 
-//meera nathan 
-
 <!DOCTYPE html>
 <html>
 <head>
     <title> Code Homework 1 </title>
+    <style>
+    body { 
+        background-color: #00c4b4;
+        color: #6c6c6c;
+        border-style: double;
+        border-width: 10px;
+        padding-left: 10px;
+
+    }
+    </style>
 </head>
 
-<body>
+<body> 
     <h1> Challenge 1: Correct Change </h1>
 
     <?php 
 
-    $inputChange = 159;
-    $dollars = (int) ($inputChange / 100);
-    $leftAfterDollars = $inputChange % 100;
-    $quarters = (int) ($leftAfterDollars / 25);
-    $leftAfterQuarters = $leftAfterDollars % 25;
-    $dimes = (int) ($leftAfterQuarters / 10);
-    $leftAfterDimes = $leftAfterQuarters % 10;
-    $nickels = (int) ($leftAfterDimes / 5);
-    $leftAfterNickels = $leftAfterDimes % 5;
-    $pennies = ($leftAfterNickels); 
+    $inputChange = 132; // to test code, change input value here 
 
-    echo "You are due " . $inputChange . " cents in change. ";
+    if (gettype($inputChange) == "integer" && $inputChange > 0) { // check for correct input type and positive number
+        //find number of dollars
+        $dollars = (int) ($inputChange / 100);
+
+        //find remainder after dollars 
+        $leftAfterDollars = $inputChange % 100;
+
+        //find number of quarters 
+        $quarters = (int) ($leftAfterDollars / 25);
+
+        //find remainder after quarters 
+        $leftAfterQuarters = $leftAfterDollars % 25;
+
+        //find number of dimes
+        $dimes = (int) ($leftAfterQuarters / 10);
+
+        //find remainder after dimes 
+        $leftAfterDimes = $leftAfterQuarters % 10;
+
+        //find number of nickles
+        $nickels = (int) ($leftAfterDimes / 5);
+
+        //find remainder after nickles 
+        $leftAfterNickels = $leftAfterDimes % 5;
+
+        //find number of pennies 
+        $pennies = ($leftAfterNickels); 
+
+        echo "You are due " . $inputChange . " cents in change. ";
 
 
-    echo "You will get back ". $dollars ." dollar bill(s), ". $quarters ." quarter(s), ". $dimes. " dime(s), ". $nickels. " nickel(s), and ". $pennies. " pennie(s)" ;
+        echo "You will get back ". $dollars ." dollar bill(s), ". $quarters ." quarter(s), ". $dimes. " dime(s), ". $nickels. " nickel(s), and ". $pennies. " pennie(s)" ;
+    
 
+    } else {
+        echo "Please input an appropriate number of cents."; // output for inappropriate inputs 
+    }
+    
 
     ?>
 
     <h1> Challenge 2: 99 Bottles of Beer </h1>
 
-    <?php
-    $bottles = 99;
-    // this answers both 2a and 2b 
-    while ($bottles > 0 ) {
-        echo $bottles. " bottles of beer on the wall. " .$bottles. " bottles of beer.";
-        $bottles--;
-        echo " Take one down, pass it around, ".$bottles. " bottles of beer on the wall."; 
-        print "<p>";
+    <img src="http://pngimg.com/uploads/beer/beer_PNG2372.png" align = "right">
 
+    <?php
+    // this answers both 2a and 2b 
+    $bottles = 99; //to test code, change input value here 
+    if (gettype($bottles)== "integer" && $bottles > 0) { //check for correct input type and positive number 
+        while ($bottles > 0 ) { //loop through number of bottles until terminating case 
+        echo $bottles. " bottles of beer on the wall. " .$bottles. " bottles of beer."; //print first half of verse
+        $bottles--; // decrement bottles
+        echo " Take one down, pass it around, ".$bottles. " bottles of beer on the wall."; //print second half of verse 
+        print "<p>";
+        }
+    
+    } else {
+        echo "Please input an appropriate number of bottles."; //output for inappropriate inputs
     }
         
 
